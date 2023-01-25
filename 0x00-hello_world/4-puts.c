dd694d90053d5424d5faaf0c304db5882d0d7f4f
AdrianWolfP <wolfy52032@gmail.com>
2023-01-25 11:07:09 -0800

    r

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
-# !/bin/bash
 #include <stdio.h>
-
+/**
+* main - Entry point
+*
+* Return: Always 0 
+*/
 int main(void)
 {
 
-	puts(" \ " "Progamming is like building a multlilngual puzzle ");
+	puts("\"Progamming is like building a multlilngual puzzle");
 	return (0); 
 
 }
