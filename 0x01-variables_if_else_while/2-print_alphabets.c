2fd82d7d29b1a4b54c271bc3ad8e84c3b6734b27
AdrianWolfP <wolfy52032@gmail.com>
2023-01-27 11:50:33 -0800

    f

---
+++ 0x01-variables_if_else_while/2-print_alphabets.c
@@ -0,0 +1,16 @@
+/**
+*main - Enrty point
+*Return: Always 0
+*/
+# include <stdio.h>
+
+int main(void)
+{
+	char c;
+	for (c= 'a'; c <= 'z'; c++)
+{
+	putchar(c);
+}
+	putchar('\n');
+	return (0);
+}
