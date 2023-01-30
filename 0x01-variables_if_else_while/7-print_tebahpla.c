f1af33564b6cd302d8013cf7582f3b47a07ac5ca
AdrianWolfP <wolfy52032@gmail.com>
2023-01-30 07:17:26 -0800

    j

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+/**
+*main - Enrty point
+*Return: Always Zero
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	char c;
+	for (c = 'z'; c >= 'a'; c--)
+{
+	putchar(c);
+}
+	putchar('\n');
+	return (0);
+}
