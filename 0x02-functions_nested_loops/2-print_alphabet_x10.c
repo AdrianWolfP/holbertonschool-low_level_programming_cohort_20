08c1b3981a6c9cddf9e35aa71aaa6f2f4ca248b7
AdrianWolfP <wolfy52032@gmail.com>
2023-01-30 21:13:27 -0800

    a

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,18 @@
 #include "main.h"
-/*
- *
- * main - Enrty point
- *
- * Return: 0
- */
-void print_alphabet(void)
+/**
+* main - Enrty point
+* Return: 0
+*/
+void print_alphabet_x10(void)
 {
-	char c;
-	for (c = 'a'; c <= 'z'; c++)
+int i;
+int c;
+for (i = 0; i < 10; i++)
+{
+for (c = 'a'; c <= 'z'; c++)
 {
 	_putchar(c);
 }
 	_putchar('\n');
+}
 }
