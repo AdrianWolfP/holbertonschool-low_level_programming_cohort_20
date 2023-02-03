b2896e24c345e0841c1a9f3ca3fdcf2f5ea275af
AdrianWolfP <wolfy52032@gmail.com>
2023-02-03 12:58:15 -0800

    u

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,8 +10,8 @@ void print_most_numbers(void)
 	int x;
 for (x = 0; x <= 9; x++)
 	{
-	if (z != 2 && z != 4)
-	_putchar (z + '0');
+	if (x != 2 && x != 4)
+	_putchar (x + '0');
 	_putchar ('\n');
 	}
 return;
