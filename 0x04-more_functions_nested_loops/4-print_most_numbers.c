9333cdbd8d97e39f33dfe3f9aefa6fb98df1da60
AdrianWolfP <wolfy52032@gmail.com>
2023-02-03 12:53:26 -0800

    d

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include"main.h"
+/**
+*main - prints out numbers 
+*Return: Always 0
+*/
+
+void print_most_numbers(void)
+{
+
+	int x;
+for (x = 0; x <= 9; x++)
+	{
+	if (z != 2 && z != 4)
+	_putchar (z + '0');
+	_putchar ('\n');
+	}
+return;
+}
