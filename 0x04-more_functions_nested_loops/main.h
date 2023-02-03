eece2404615833970201f1ff17a2fbf38cfdc2ec
AdrianWolfP <wolfy52032@gmail.com>
2023-02-03 09:14:55 -0800

    j

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,10 +1,5 @@
-#include <stdio.h>
 #ifndef _PUTCHAR
 #define _PUTCHAR
-/**
-*main - Enrty point
-*Return: Always 0
-*/
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -15,3 +10,5 @@ void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
+void print_triangle(int size);
+#endif
