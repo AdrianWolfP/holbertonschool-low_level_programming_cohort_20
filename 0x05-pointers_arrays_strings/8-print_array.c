4cf7ce61f1be26283cfdf1f7e823c7e710d1c5ed
AdrianWolfP <wolfy52032@gmail.com>
2023-02-06 21:24:40 -0800

    i

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*print_array = prints out n elements of an array of integers
+*@a: pointer to first int
+*@n: pointer to second int
+*Return: Always 0
+*/
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == 0)
+			printf("%d", a[i]);
+		else
+			printf(", %d", a[i]);
+	}
+	printf("\n");
+}
