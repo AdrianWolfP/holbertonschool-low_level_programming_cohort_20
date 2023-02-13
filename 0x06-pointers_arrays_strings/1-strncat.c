c7e1495b2bc538d8835d68c8a7eca6c61ff17d4f
AdrianWolfP <wolfy52032@gmail.com>
2023-02-13 08:18:58 -0800

    d

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,13 +14,12 @@ char *_strncat(char *dest, char *src, int n)
 	int len = 0;
 	int index;
 
-	while (dest[index++]
-	{
+	while (dest[index++])
+	
 		len++;
-	}
+	
 
 	for (index = 0; src[index] && index < n; index++)
-		dest[len + i] = src[i];
 	dest[len++] = src[index];
 
 	return (dest);
