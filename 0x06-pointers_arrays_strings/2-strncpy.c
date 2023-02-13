b2ff5797b8e0e4ddc827adf5cfe8c5c1646de117
AdrianWolfP <wolfy52032@gmail.com>
2023-02-13 08:29:26 -0800

    e

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+*_strncpy - function that copies a string
+*@dest: destination char
+*@src: source char
+*@n: number of bytes from src
+*Return: New string
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int index = 0, src_len = 0;
+
+	while (src[index++])
+	src_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+	dest[index] = src[index];
+
+	for (index = src_len; index < n; index++)
+	dest[index] = '\0';
+
+	return (dest);
+}
