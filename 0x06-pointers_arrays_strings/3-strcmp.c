c7bcd84fe4a8e743105dfe86a3cd721612cc15b2
AdrianWolfP <wolfy52032@gmail.com>
2023-02-13 08:38:32 -0800

    g

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+*_strcmp - function that compares two strings
+*@s1: string one
+*@s2: string two
+*Return: 0 if strings are correct
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
