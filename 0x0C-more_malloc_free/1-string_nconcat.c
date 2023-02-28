b35df94f4a0428b1cbf6d5ae01a022d807b27341
AdrianWolfP <wolfy52032@gmail.com>
2023-02-27 19:10:04 -0800

    d

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,37 +12,32 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 
-	unsigned int s1Len = 0, s2Len = 0, bufferLen, index = 0;
-	char *pointer;
+	unsigned int len = n, index;
+	char *concat;
 
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
 		s2 = "";
-	while (s1[s1Len])
-		s1Len++;
-	while (s2[s2Len])
-		s2Len++;
-	if (s2Len > n)
-		s2Len = n;
-	bufferLen = s1Len + s2Len;
 
-	pointer = malloc(sizeof(char) * (bufferLen + 1));
-	if (pointer == NULL)
-
-		return (NULL);
-	s1Len = 0;
 	for (index = 0; s1[index]; index++)
-	{
-		pointer[s1Len] = s1[index];
-		s1Len++;
-	}
-	for (index = 0; s2[index] && index < bufferLen; index++)
-	{
-		pointer[s1Len] = s2[index];
-		s1Len++;
-		s2Len++;
-	}
-	pointer[s1Len] = '\0';
-	return (pointer);
+		len++;
+
+	concat = malloc(sizeof(char) * (len + 1));
+
+	if (concat == NULL)
+		return (NULL);
+
+	len = 0;
+
+	for (index = 0; s1[index]; index++)
+		concat[len++] = s1[index];
+
+	for (index = 0; s2[index] && index < n; index++)
+		concat[len++] = s2[index];
+
+
+	concat[len] = '\0';
+	return (concat);
 }
