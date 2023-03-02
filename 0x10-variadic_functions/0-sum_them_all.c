eca781cb28ff566c30f76979047c8aa7004529b4
AdrianWolfP <wolfy52032@gmail.com>
2023-03-02 13:04:19 -0800

    h

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,9 +1,9 @@
 #include "variadic_functions.h"
 /**
  * sum_them_all - adds all passed in after n
- * @n: counts 
+ * @n: counts
  * Return: Always 0
- */ 
+ */
 
 int sum_them_all(const unsigned int n, ...)
 {
