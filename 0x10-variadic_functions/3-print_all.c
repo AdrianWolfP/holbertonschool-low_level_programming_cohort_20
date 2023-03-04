5d0f9477b8ad9081d8c95fe559f5dd8923462781
AdrianWolfP <wolfy52032@gmail.com>
2023-03-04 15:57:39 -0800

    r

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,47 @@
+#include "variadic_functions.h"
+/**
+ *print_all - prints anything
+ *@format: list of arguments
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list ap, check;
+	char cin, *sin, *seg = ", ";
+	int iin, x = 0;
+	double fin;
+
+	va_start(ap, format);
+	va_start(check, format);
+
+	while (format != NULL && format[x] != '\0')
+	{
+		if (format[x + 1] == '\0')
+			seg = "";
+		switch (format[x])
+		{
+			case 'c':
+				cin = va_arg(ap, int);
+				printf("%c%s", cin, seg);
+				break;
+			case 'i':
+				iin = va_arg(ap, int);
+				printf("%d%s", iin, seg);
+				break;
+			case 'f':
+				fin = va_arg(ap, double);
+				printf("%f%s", fin, seg);
+				break;
+			case 's':
+				sin = va_arg(ap, char *);
+				if (sin == NULL)
+					sin = "(nil)";
+				printf("%s%s", sin, seg);
+				break;
+			default:
+				break;
+		}
+		x++;
+	}
+	printf("\n");
+}
