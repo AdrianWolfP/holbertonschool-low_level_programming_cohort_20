77c13dbf38537f9caab27531739d53e7186c62ec
AdrianWolfP <wolfy52032@gmail.com>
2023-03-04 20:14:33 -0800

    t

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,37 @@
+#include "3-calc.h"
+/**
+ *main - calculates math problems
+ *@argc: command line count
+ *@argv: argument vector
+ *Return: Always 0
+ */
+
+int main(int argc, char **argv)
+{
+	int num1, num2;
+	char *operator;
+	int (8op)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	operator = argv[2];
+
+	op = get_op_func(operator);
+
+	if (op == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((strcmp(operator, "/") == 0 || (strcmp(operator, "%s")) == 0) && num2 == 0)
+	{
+		printf("%d\n", op(num1, num2));
+
+		return (0);
+}
