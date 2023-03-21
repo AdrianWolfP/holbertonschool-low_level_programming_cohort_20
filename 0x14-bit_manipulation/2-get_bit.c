c2fb1d0092a0ee36448aa99a67a7dd76896ab0d4
AdrianWolfP <wolfy52032@gmail.com>
2023-03-20 21:56:25 -0700

    f

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- *  * get_bit - returns the value of a bit at a given index
- *   * @n: number to find
- *    * @index: the index you wish to calculate
- *     * Return: value of bit at index or `-1` if an error occurs
- *      */
+ * get_bit - returns the value of a bit at a given index
+ * @n: number to find
+ * @index: the index you wish to calculate
+ * Return: value of bit at index or `-1` if an error occurs
+ */
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int i = 0;
--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- *  * set_bit - sets the value of a bit to 1 at a given index
- *   * @n: input number
- *    * @index: bit to change
- *     * Return: 1 upon success, -1 otherwise
- *      */
+ * set_bit - sets the value of a bit to 1 at a given index
+ * @n: input number
+ * @index: bit to change
+ * Return: 1 upon success, -1 otherwise
+ */
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > sizeof(long) * 8)
